b18644e3a576fdc6298d44c02b9f5c764daf5ad8
Sai Abbirame Ramanathan <1967747@edu.vaniercollege.qc.ca>
2026-04-22 10:35:48 -0400

    Add project files

---
+++ class01/main.py
@@ -0,0 +1,29 @@
+print("hello world")
+
+num1 = 5
+num2 = 10
+
+print(num1 + num2)
+
+#this is a function!
+def my_function(num1, num2):
+    return num1 + num2
+
+if (2 > 3):
+    print("This is a line")
+    print("This is another line!")
+    print("True!")
+    if(5 > 3):
+        print("inside!")
+else:
+    print("False!")
+
+for i in range(0, 5):
+    print("Loop")
+
+class MyClass():
+    x = 25
+
+myClass1 = MyClass()
+
+print(myClass1.x)
