ca5e444ae6272ee9df2e542ac03b861af81fbe1d
Sai Abbirame Ramanathan <1967747@edu.vaniercollege.qc.ca>
2026-05-12 10:20:17 -0400

    Change the display_info format on main

--- class06/lab/library-item-tracker/book.py
+++ class06/lab/library-item-tracker/book.py
@@ -7,9 +7,7 @@ class Book:
         self.available = available
 
     def display_info(self):
-        print(f"Title: {self.title}")
-        print(f"Author: {self.author}")
-        print(f"Available: {self.available}")
+        print(f"{self.title} by {self.author} is Available: {self.available}")
 
     def borrow(self):
         if (self.available == False):
