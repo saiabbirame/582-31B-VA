b24ac178789a1740487fe09b6b0333f5019d8171
Sai Abbirame Ramanathan <1967747@edu.vaniercollege.qc.ca>
2026-05-12 09:29:49 -0400

    Add base implementation

---
+++ class06/lab/library-item-tracker/main.py
@@ -0,0 +1,12 @@
+from book import Book
+
+book1 = Book("The Cruel Prince", "Holly Black", True)
+book2 = Book("Atomic Habits", "James Clear", False)
+
+print(book1.title)
+print(book1.author)
+print(book1.available)
+
+print(book2.title)
+print(book2.author)
+print(book2.available)
