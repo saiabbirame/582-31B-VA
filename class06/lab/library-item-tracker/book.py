e2ca99dfe1363d1d34837e0bea2f1de6d592056e
Sai Abbirame Ramanathan <1967747@edu.vaniercollege.qc.ca>
2026-05-12 09:56:19 -0400

    Add borrow and return_book instance methods

--- class06/lab/library-item-tracker/book.py
+++ class06/lab/library-item-tracker/book.py
@@ -8,3 +8,14 @@ class Book:
         print(f"Title: {self.title}")
         print(f"Author: {self.author}")
         print(f"Available: {self.available}")
+
+    def borrow(self):
+        if (self.available == False):
+            print(f"{self.title} is already borrowed.")
+        else: 
+            self.available = False
+            print(f"{self.title} has been borrowed.")
+
+    def return_book(self):
+        self.available = True
+        print(f"{self.title} has been returned.")
