40422d9e324a71c016f7daae5213654a352ce5db
Sai Abbirame Ramanathan <1967747@edu.vaniercollege.qc.ca>
2026-05-12 09:54:49 -0400

    Add display_info instance method

--- class06/lab/library-item-tracker/book.py
+++ class06/lab/library-item-tracker/book.py
@@ -1,5 +1,10 @@
 class Book:
-    def __init__(self, title, author, available):
+    def __init__(self, title, author, available = True):
         self.title = title
         self.author = author
         self.available = available
+
+    def display_info(self):
+        print(f"Title: {self.title}")
+        print(f"Author: {self.author}")
+        print(f"Available: {self.available}")
--- class06/lab/library-item-tracker/main.py
+++ class06/lab/library-item-tracker/main.py
@@ -6,7 +6,13 @@ book2 = Book("Atomic Habits", "James Clear", False)
 print(book1.title)
 print(book1.author)
 print(book1.available)
+print()
 
 print(book2.title)
 print(book2.author)
 print(book2.available)
+print()
+
+
+
+
