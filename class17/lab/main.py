de35ae3609409e4cf2483830048b2e569646f6f0
Sai Abbirame Ramanathan <1967747@edu.vaniercollege.qc.ca>
2026-06-16 10:57:27 -0400

    Add staff model and use it in main

--- class17/lab/main.py
+++ class17/lab/main.py
@@ -2,13 +2,16 @@ from core.constants import MAX_TICKETS_PER_BOOKING
 from core.enums import ShowStatus
 from core.exceptions import InvalidBookingError
 from models.customer import Customer
+from models.staff import Staff
 from models.movie_show import MovieShow
 
 def main():
     customer = Customer("Ava")
+    staff = Staff("Alex")
     show = MovieShow("Inception", 20, ShowStatus.OPEN)
 
     customer.display_info()
+    staff.display_info()
     show.display_info()
     print("Max tickets per booking:", MAX_TICKETS_PER_BOOKING)
 
---
+++ class17/lab/models/staff.py
@@ -0,0 +1,6 @@
+class Staff:
+    def __init__(self, name):
+        self.name = name
+
+    def display_info(self):
+        print(f"Staff: {self.name}")
